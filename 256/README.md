5013a34400b0ebcd948b10f17941ca723649b9b0
Alan-Liang <a@keeer.net>
2019-10-04 10:22:54 +0800

    add 256

---
+++ 256/README.md
@@ -0,0 +1,15 @@
+# 256
+![](https://alan.liangcn.ml/CodeStyle.svg)
+
+The 256 game.
+
+### Rules
+Just like 2048, but simpler.  
+You'll have to choose a place to set up the new tile 2, and then use wasd, the arrow keys, or swipe to move the map towards a certain direction. And then... Loop, until you cannot move anymore. If you (maybe once) reached all of the target number(s), then you win!
+
+### License
+GPLv3, see [`LICENSE`](https://github.com/KEEER/256/blob/master/LICENSE) for details.
+
+### Acknowledgements
+- This game is inspired by [2048](https://github.com/gabrielecirulli/2048).
+- This game is hosted on [Github Pages](https://pages.github.com/).
---
+++ 256/sw.js
@@ -0,0 +1,25 @@
+var cacheName = '256Gamev0.1.2';
+var appShellFiles = ['/256/','/256/index.html','/256/icon.png'];
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
